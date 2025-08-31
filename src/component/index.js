6cb7ba72c4f3a745f5d332b71516b07eed8db656
Tishil2045 <tishil20042005@gmai.com>
2025-08-31 17:53:53 +0530

    Fix case sensitivity issue for LogoutBtn

--- src/component/index.js
+++ src/component/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Container from './container/Container'
 import Logo from "./Logo";
-import LogoutBtn from "./Header/Logoutbtn";
+import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input"
 import RTE from "./RTE";
